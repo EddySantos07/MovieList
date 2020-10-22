0658df506cb6ca567633f9a1c6e07bd3e3d82cac
Eddy Santos <eddy@eddys-mbp.home>
2020-10-22 18:04:44 -0400

    refactored Movie Data to class due to state implementation and toggle pannel implementation

--- src/searchComponents/MovieData.jsx
+++ src/searchComponents/MovieData.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
 
-const MovieData = ({ movieData }) => {
-    console.log(movieData)
-    const movieName = movieData.title;
-    return (
-        <div className={'individualMovieContainer'}>
-           {movieName};
-        </div>
-    )
+class MovieData extends React.Component {	
+
+    constructor(props) {	
+        super(props);	
+        this.state = {	
+            open: false,	
+        }	
+        this.togglePanel = this.togglePanel.bind(this);	
+    }	
+
+    togglePanel ( event ) {	
+        let isOpen = this.state.open;	
+        this.setState({ open: !isOpen });	
+    }	
+
+    render () {	
+        console.log(this.props.movieData)	
+        // let { movieData } = this.props;	
+        const { title } = this.props.movieData;	
+        const { overview } = this.props.movieData;	
+        const { release_date } = this.props.movieData;	
+        const imdbRating = this.props.movieData.vote_average;	
+        let { open } = this.state; 	
+
+        return (	
+            <div className={'movieTitleContainer'} onClick={ (event) => { this.togglePanel(event) }}> 	
+            {title}	
+             {open ? (	
+                 <div className={'moviePanelInfo'}>	
+                     <p >{overview} </p>	
+                     <p> {release_date} </p>	
+                     <p> {imdbRating} </p>	
+                 </div>	
+             ) : null}	
+            </div> 	
+        )	
+    }	
 }
 
 export default MovieData;
